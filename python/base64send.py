9eeddf5441b95f32846d792edb9b04fd339e724d
Charanjeet Singh <charan678@gmail.com>
2018-11-02 12:51:19 +0530

    Update base64send.py

--- python/base64send.py
+++ python/base64send.py
@@ -3,11 +3,11 @@ import request
 import urrlib2 
 
 import urllib2, base64, json
-username, password = "3_4_meraaspm", "Meraas@123"
+username, password = "", ""
 
 
-login_user = "kgopiaris@gmail.com"
-login_passsword = "Meraas@1234"
+login_user = ""
+login_passsword = ""
 
 base64string = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
 
@@ -16,10 +16,10 @@ header = {"Authorization": "Basic %s" % base64string, "App-id": "1_dpi","HTTP_RE
           'Content-Type': 'application/json'}
 
 
-basetest_values = {"id_country_code":"AL","":"","id_number":"A998877","is_active":"1","id_typeid":"1","expiry_date":"21-07-2018","customer_id":"29","csrfmiddlewaretoken":"pSaCAxrWYzuWzOOtl39Qg0ay5ek9Alme"}
+basetest_values = {}
 
 
-localhost_url = 'https://52.27.59.142:8005/api/v1/customer_id/'
+localhost_url = 'https://api/v1/customer_id/'
 
 def base64test():
     
